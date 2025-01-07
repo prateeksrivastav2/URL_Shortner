b58848137556d02948c15ee7b1e069e32056918d
prateeksrivastav2 <111221882+prateeksrivastav2@users.noreply.github.com>
2025-01-07 10:50:44 +0530

    Rate Limiting Done

--- api/routes/shortner.go
+++ api/routes/shortner.go
@@ -1,10 +1,14 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/asaskevich/govalidator"
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"github.com/prateeksrivastav2/UrlShortner/api/database"
 	"github.com/prateeksrivastav2/UrlShortner/api/helpers"
 )
 
@@ -31,6 +35,23 @@ func ShortenURL(c *fiber.Ctx) error {
 	}
 
 	// Rate Limiting
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+	value, err := r2.Get(c.IP()).Result()
+	if err == redis.Nil {
+		_ = r2.Set(c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		value, _ = r2.Get(c.IP()).Result()
+		valInt, _ := strconv.Atoi(value)
+		if valInt <= 0 {
+			limit, _ := r2.TTL(c.IP()).Result()
+			return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{
+				"error":            "Rate limit exceeded",
+				"rate_limit_reset": limit / time.Nanosecond / time.Minute,
+			})
+		}
+	}
+
 	// input is url or not
 	if !govalidator.IsURL(body.URL) {
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
@@ -45,6 +66,6 @@ func ShortenURL(c *fiber.Ctx) error {
 	}
 
 	body.URL = helpers.EnforceHTTP(body.URL)
-
+	r2.Decr(c.IP())
 	return nil
 }
